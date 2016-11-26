c57f4696bbc8cdacc672457a4b583a1b4df7a3fe
DESKTOP-J56R2T4\TOSHIBA <martincevic.alen@gmail.com>
2016-11-26 15:15:45 +0100

    Dodano facebook, twitter log in, google zajebva

---
+++ Glavna_aplikacija/app/src/main/java/com/hiphiparray/quizify/LogInActivity.java
@@ -0,0 +1,52 @@
+package com.hiphiparray.quizify;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+/**
+ * Created by TOSHIBA on 26.11.2016..
+ */
+
+public class LogInActivity extends AppCompatActivity {
+
+    public TwitterLoginButton loginButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+
+
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+    }
+}
---
+++ Glavna_aplikacija/app/src/main/java/com/hiphiparray/quizify/MyApplication.java
@@ -0,0 +1,52 @@
+package com.hiphiparray.quizify;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by TOSHIBA on 26.11.2016..
+ */
+
+public class MyApplication extends Application{
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "GOtHZKM6XThhw5ZIXcd5JzUFm";
+    private static final String TWITTER_SECRET = "f8XuYKEOC6mmMAfvAoc0zwM1pR0BvgQosCKIZCg6HgPI8SIbzk";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        printHashKey();
+    }
+
+
+    public void printHashKey(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.hiphiparray.quzify",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
