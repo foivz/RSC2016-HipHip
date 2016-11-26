077d95084be573b4304434ef0f8e690abcca8a98
Antonio Lopac <lopac.antonio@outlook.com>
2016-11-26 11:38:11 +0100

    Start project

---
+++ QuizifyWeb/QuizifyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizifyWeb.Startup))]
+namespace QuizifyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
